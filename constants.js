a83a866246635e190d8c00581068427521ebca35
Chris Falch <chris.falch@amfam.com>
2018-12-02 12:20:19 -0600

    all the things

---
+++ constants.js
@@ -0,0 +1,27 @@
+var GLYPHS = [
+	'alien', 'angry', 'axe', 'axes', 'blush', 'bunny', 'butterfly', 'butterflyblue', 
+	'cake', 'cat', 'catgrey', 'coffee', 'cool', 'crying', 'dog', 'donut', 'elementdark', 
+	'elementfire', 'elementholy', 'elementice', 'elementnature', 'evil', 'fire', 'flower', 
+	'fox', 'geek', 'ghost', 'grin', 'hammer', 'hankey', 'heart', 'heartblue', 'heartgreen', 
+	'heartpurple', 'heartyellow', 'helmet', 'horror', 'kiss', 'lightning', 'lol', 'love', 
+	'mage', 'magered', 'monocle', 'palmtree', 'pizza', 'richard', 'rofl', 'rolleyes', 'sad', 
+	'skull', 'smile', 'splash', 'staff', 'star', 'sunflower', 'sword', 'swords', 'teardrop', 
+	'think', 'thumbsdown', 'thumbsup', 'tongue'
+];
+var COLORS = {
+	'White': '[#FFFFFF]',
+	'DeepSkyBlue': '[#00BFFF]',
+	'SeaGreen': '[#2E8B57]',
+	'Red': '[#FF0000]',
+	'Orange': '[#FFA500]',
+	'Gold': '[#FFD700]',
+	'DarkOrange': '[#FF8C00]',
+	'Yellow': '[#FFFF00]',
+	'Lime': '[#00FF00]',
+	'Aqua': '[#00FFFF]',
+	'Fuchsia': '[#FF00FF]',
+	'DodgerBlue': '[#1E90FF]'
+};
+var ASCII = [
+	'¯\\_(ツ)_/¯', 'ツ', '☃'
+]; // more at http://asciimoji.com/
